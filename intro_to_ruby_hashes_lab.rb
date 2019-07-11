49e21952c735d7209185d2386d69d19c705a0b4c
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 20:18:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,10 +12,13 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => 
-  	{:railroads =>
-    	{:pieces => 4
-    	}
+	monopoly = {
+  	  :railroads =>
+      	{
+      	  :pieces => 4
+      	}
+      :names => {}
+      :rent_in_dollars => {}
     }
   }
 end
