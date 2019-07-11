a29f6d4823c66399e8f3000b1b6367a002b6e50b
Alice Brunel <alc.brunel@gmail.com>
2019-07-11 20:25:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
   	  :railroads =>
       	{
       	  :pieces => 4
-      	}
+      	},
       :names => {
         :reading_railroad =>
           {},
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
           {},
         :shortline =>
           {}
-      }
+      },
       :rent_in_dollars => {
         :one_piece_owned => 25,
         :two_pieces_owned => 50,
